bb103eff2d8cadeafe79ad0289b149722b21c874
Kae Lucas <dev.kaeteodoro@gmail.com>
2025-11-22 14:51:19 -0300

    Adding backend

---
+++ app-moniflow/src/main/java/com/moniflow/app_moniflow/AppMoniflowApplication.java
@@ -0,0 +1,13 @@
+package com.moniflow.app_moniflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMoniflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppMoniflowApplication.class, args);
+	}
+
+}
---
+++ app-moniflow/src/test/java/com/moniflow/app_moniflow/AppMoniflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moniflow.app_moniflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMoniflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
